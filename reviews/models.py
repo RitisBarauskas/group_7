7c2b8616e860cd74300720bfbd2b5802bf93ef6b
Ritis Barauskas <perm300ru@yandex.ru>
2024-05-14 19:26:03 +0300

    <YANDEX-3>: upd model Comment, created constants.py

--- reviews/models.py
+++ reviews/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
+from backend.constants import MAX_LENGTH_CHAR_FIELD
+
 
 class Comment(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=MAX_LENGTH_CHAR_FIELD, verbose_name='Имя')
+    email = models.EmailField(verbose_name='Электронная почта')
+    text = models.TextField(verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
