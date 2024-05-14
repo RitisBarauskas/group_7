eaba4e9fcd09243938242bfb2ef2ae7b5b3b134c
Ritis Barauskas <perm300ru@yandex.ru>
2024-05-14 19:28:23 +0300

    <YANDEX-2>: upd model Review, added constants.py

--- reviews/models.py
+++ reviews/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
+from backend.constants import MIN_RATING, MAX_LENGTH_CHAR_FIELD
+
 
 class Review(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=MAX_LENGTH_CHAR_FIELD)
     text = models.TextField()
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=MIN_RATING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+    def __str__(self):
+        return self.name
