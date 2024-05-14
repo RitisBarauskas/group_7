8d0a0f611b33b81871409c56387c6ca0e92a9de0
Ritis Barauskas <perm300ru@yandex.ru>
2024-05-14 19:21:05 +0300

    <YANDEX-3>: added model Comment

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
